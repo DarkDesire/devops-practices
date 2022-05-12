b60c353b2748f11bcd30943b72e596cf7aac141d
Eldar Dragomir <oh.eldarkaa@gmail.com>
2022-05-12 21:37:38 +0400

    Create solution45.sh

---
+++ os-and-linux-basics/solution45.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "Would you like to sort the processes output by memory or CPU? (mem/cpuy) "
+read sortby
+echo -n "How many results do you want to display? "
+read lines
+
+if [ "$sortby" = "mem" ]
+then
+    ps aux --sort -rss | grep -i `whoami` | head -n "$lines"
+elif [ "$sortby" = "cpu" ]
+then
+    ps aux --sort -cpu | grep -i `whoami` | head -n "$lines"
+else
+    echo "No input provided. Exiting"
+fi
